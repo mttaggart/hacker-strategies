2618de9ab9a8160335ba95f0250d3febfcaa3233
Michael Taggart <michael.t.taggart@gmail.com>
2021-04-11 02:02:13 -0700

    Remove unnecessary sudo

--- update_secrets.sh
+++ update_secrets.sh
@@ -2,11 +2,11 @@
 
 docker stack rm hacker_strats
 
-sudo docker secret rm hs_ssl_cert
-sudo docker secret rm hs_ssl_key
+docker secret rm hs_ssl_cert
+docker secret rm hs_ssl_key
 
 sudo letsencrypt renew
 
-sudo docker secret create hs_ssl_cert /etc/letsencrypt/live/taggart-tech.com/fullchain.pem
-sudo docker secret create hs_ssl_key /etc/letsencrypt/live/taggart-tech.com/privkey.pem
+docker secret create hs_ssl_cert /etc/letsencrypt/live/taggart-tech.com/fullchain.pem
+docker secret create hs_ssl_key /etc/letsencrypt/live/taggart-tech.com/privkey.pem
 
