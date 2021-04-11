2691c9a8485039dae4e681a462fcfd7d49062678
Michael Taggart <michael.t.taggart@gmail.com>
2021-04-11 01:52:45 -0700

    Update for stack usage

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+docker build -t "hacker-strategies_api:latest" api/ 
+docker build -t "hacker-strategies_nginx:latest" frontend/
+
 docker stack deploy --compose-file docker-compose.yml hacker_strats
