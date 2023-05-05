6f9e0338315c4becfcc1538fae556dd5b85bba68
Michael Taggart <mtaggart@taggart-tech.com>
2023-05-04 18:06:21 -0700

    Remove last vestiges of Docker version

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stack rm hacker_strats
-
-docker build --no-cache -t "hacker-strategies_api:latest" api/ 
-docker build --no-cache -t "hacker-strategies_nginx:latest" frontend/
-
-docker stack deploy --compose-file docker-compose.yml hacker_strats
