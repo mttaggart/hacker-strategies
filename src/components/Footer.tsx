112dded05631bef63a87bb5dfb5882609b175fd5
Michael Taggart <mtaggart@taggart-tech.com>
2023-05-04 18:04:52 -0700

    Add GH link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
     <footer>
         <p>Inspiration for when you're stuck</p>
         <p>Inspired by Brian Eno's <a href="https://en.wikipedia.org/wiki/Oblique_Strategies">Oblique Strategies</a></p>
-        <p>Made with 💜 by <a href="https://taggartinstitute.org">The Taggart Institute</a></p>
+        <p><a href="https://github.com/mttaggart/hacker-strategies">Made</a> with 💜 by <a href="https://taggartinstitute.org">The Taggart Institute</a></p>
         <p><img src={ttiAvatar}/></p>
     </footer>
 );
